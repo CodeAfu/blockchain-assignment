c07b566b9b1771fe5e12c1735e199c9532e6ac95
CodeAfu <ibrahim.afzal1999@gmail.com>
2025-06-08 20:28:36 +0800

    Declared MetaMaskInpageProvider in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
 import Header from "@/components/header";
+import { MetaMaskInpageProvider } from "@metamask/providers";
 
 const inter = Inter({ subsets: ["latin"] });
+declare global {
+  interface Window {
+    ethereum?: MetaMaskInpageProvider;
+  }
+}
 
 export const metadata: Metadata = {
   title: "MediaVault",
