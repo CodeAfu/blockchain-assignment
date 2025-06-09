14ab67ac6aa27a4f6491d95f0277ae05d7bddb44
Ibrahim Afzal Shareef <127954747+CodeAfu@users.noreply.github.com>
2025-06-09 17:19:18 +0800

    Feature/media contract (#8)
    * Added sample component that interacts successfully with smart contract
    
    * RE: Added sample component that interacts successfully with smart contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
-        Main
-      </main>
+    <div>
+      <main>Main</main>
       <footer className="">Footer</footer>
     </div>
   );
