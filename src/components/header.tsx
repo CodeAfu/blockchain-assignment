57c6502138c0241948ed596c9e8f565bcc049530
CodeAfu <ibrahim.afzal1999@gmail.com>
2025-06-14 11:47:34 +0800

    Integrated wallet info component and patched up the UI design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,25 @@ import { Button } from "./shadcn-ui/button";
 import Wallet from "./wallet";
 import { WalletProvider } from "@/contexts/wallet-context";
 
+const navLinks = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Register Media",
+    href: "/media/register",
+  },
+  {
+    label: "Sample",
+    href: "/sample",
+  },
+  {
+    label: "Test",
+    href: "/test",
+  },
+];
+
 export default function Header() {
   return (
     <nav className="flex items-center justify-center p-4 bg-slate-500/10 h-20">
@@ -14,21 +33,28 @@ export default function Header() {
           <Link href="/" className="">
             <div className="flex items-center space-x-4">
               <Cuboid className="h-12 w-12" />
-              <h1 className="text-lg font-semibold">Blockchain</h1>
+              <h1 className="text-2xl font-semibold tracking-wider">
+                <span className="relative">
+                  <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-primary to-purple-950">
+                    Media
+                  </span>
+                  <span>Vault</span>
+                  <div className="absolute -bottom-0.5 left-0 w-20 h-0.5 bg-gradient-to-r from-primary to-purple-600 rounded-full"></div>
+                </span>
+              </h1>
             </div>
           </Link>
+
+          {/* Navigation */}
           <div className="space-x-4">
-            <Button asChild variant="link" className="p-0 text-foreground">
-              <Link href="/">Home</Link>
-            </Button>
-            <Button asChild variant="link" className="p-0 text-foreground">
-              <Link href="/media">Register Media</Link>
-            </Button>
-            <Button asChild variant="link" className="p-0 text-foreground">
-              <Link href="/sample">Sample</Link>
-            </Button>
+            {navLinks.map((item, index) => (
+              <Button key={index} variant="link" className="p-0 text-foreground text-md">
+                <Link href={item.href}>{item.label}</Link>
+              </Button>
+            ))}
           </div>
         </div>
+
         {/* Right */}
         <WalletProvider>
           <Wallet />
