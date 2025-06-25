fd5379ebeb6722fc89a5a897441b3010b1a72f54
CodeAfu <ibrahim.afzal1999@gmail.com>
2025-06-25 18:46:26 +0800

    Removed registerMedia call on sample component

--- src/app/sample/_components/sample-component.tsx
+++ src/app/sample/_components/sample-component.tsx
@@ -1,38 +1,13 @@
 "use client";
 
-import { Button } from "@/components/shadcn-ui/button";
-import { useMediaContract } from "@/hooks/use-media-contract";
 import { useAppKitAccount } from "@reown/appkit/react";
-import { ethers } from "ethers";
 import React, { useRef } from "react";
 
 export default function SampleComponent() {
   const titleRef = useRef<HTMLInputElement | null>(null);
   const ipfsRef = useRef<HTMLInputElement | null>(null);
   const royaltyRef = useRef<HTMLInputElement | null>(null);
-  const contract = useMediaContract();
   const { isConnected } = useAppKitAccount();
-
-  const registerMedia = async () => {
-    const title = titleRef.current?.value;
-    const ipfsHash = ipfsRef.current?.value;
-    const royaltyInput = royaltyRef.current?.value;
-
-    if (!title || !ipfsHash || !royaltyInput) {
-      alert("Please fill all fields");
-      return;
-    }
-
-    const royaltyFee = ethers.parseEther(royaltyInput); // Converts from "0.01" -> wei
-
-    try {
-      if (!contract) return;
-      await contract.registerMedia(title, ipfsHash, royaltyFee);
-    } catch (error) {
-      console.error("Registration failed:", error);
-      alert("Error Registering Media. See console logs for more info");
-    }
-  };
 
   if (!isConnected)
     return (
@@ -52,10 +27,6 @@ export default function SampleComponent() {
           <label htmlFor="title">Royalty Fee:</label>
           <input name="title" ref={royaltyRef} type="number" className="p-1" />
         </form>
-        Test Smart Contract
-        <Button variant="secondary" onClick={registerMedia}>
-          Register
-        </Button>
       </div>
     </div>
   );
